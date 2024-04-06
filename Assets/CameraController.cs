3729245ae8fa2ea0247dde50ad905fac68a21efb
ZiEMBACZK <officialkacperziembinski@gmail.com>
2024-04-06 14:53:59 +0200

    commit

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,43 +6,51 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField]Transform windowsOne;
-    [SerializeField]Transform windowsTwo;
-    [SerializeField]Transform windowsThree;
+    [SerializeField] Transform windowsOneTrans;
+    [SerializeField] Transform windowsTwoTrans;
+    [SerializeField] Transform windowsThreeTrans;
+    [SerializeField] Transform roomDefTrans;
     [SerializeField] Camera cam;
+    [SerializeField] float time;
 
     void Update()
-     {
-            //Check for mouse click 
-            if (Input.GetMouseButtonDown(0))
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // Left click
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
             {
-                RaycastHit raycastHit;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out raycastHit, 100f))
-                {
-                    if (raycastHit.transform != null)
-                    {
-                       //Our custom method. 
-                        CurrentClickedGameObject(raycastHit.transform.gameObject);
-                    }
-                }
+                GameObject clickedObject = hit.collider.gameObject;
+                Debug.Log(gameObject);
+                CurrentClickedGameObject(clickedObject);
+                // Handle interaction based on the clickedObject
             }
-     }
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            cam.transform.DOMove(new Vector3(roomDefTrans.position.x, roomDefTrans.position.y, roomDefTrans.position.z), time );
+
+        }
+    }
 
     public void CurrentClickedGameObject(GameObject gameObject)
     {
-        if(gameObject.tag=="windows1")
+        Debug.Log(gameObject.tag + " " + gameObject.name);
+        if(gameObject.tag=="WindowOne")
         {
+            cam.transform.DOMove(new Vector3(windowsOneTrans.position.x, windowsOneTrans.position.y, windowsOneTrans.position.z), time );
         }
         Debug.Log(gameObject.tag + " " + gameObject.name);
-        if(gameObject.tag=="windows1")
+        if (gameObject.tag == "WindowTwo") 
         {
-            Do
+            cam.transform.DOMove(new Vector3(windowsTwoTrans.position.x, windowsTwoTrans.position.y, windowsTwoTrans.position.z), time );
         }
         Debug.Log(gameObject.tag + " " + gameObject.name);
-        if(gameObject.tag=="window1")
+        if(gameObject.tag=="WindowThree")
         {
+            cam.transform.DOMove(new Vector3(windowsThreeTrans.position.x, windowsThreeTrans.position.y, windowsThreeTrans.position.z), time );
         }
-        Debug.Log(gameObject.tag + " " + gameObject.name);
     }
 }
