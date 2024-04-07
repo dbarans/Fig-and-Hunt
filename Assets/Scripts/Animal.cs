1ed587e752fb5faeaf77c2c3ba016e58b0c87378
Dominik Barański <dbaranski@edu.cdv.pl>
2024-04-07 13:21:06 +0200

    update animal prefab

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject targetTree;
+    [SerializeField] private TreeManager treeManager;
+    [SerializeField] private float speed = 3f; 
+
     void Start()
     {
+        if (treeManager == null)
+        {
+            treeManager = FindObjectOfType<TreeManager>();
+            if (treeManager == null)
+            {
+                Debug.LogError("TreeManager reference not set in Animal script!");
+                return;
+            }
+        }
+
+        GameObject[] trees = treeManager.trees;
+        if (trees.Length > 0)
+        {
+            int randomIndex = Random.Range(0, trees.Length);
+            targetTree = trees[randomIndex];
+          
+        }
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (targetTree != null)
+        {
+      
+            Vector3 direction = (targetTree.transform.position - transform.position).normalized;
+
+            transform.Translate(direction * speed * Time.deltaTime);
+        }
     }
 }
